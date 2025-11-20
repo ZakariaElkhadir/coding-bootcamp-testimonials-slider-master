beceb48af4ce3b6fc21cc996975737c6c9030835
ZakariaElkhadir <zelkhadir5@gmail.com>
2025-11-20 17:40:31 +0100

    Add front-end style guide and initial CSS styles
    - Created a style guide outlining layout, colors, and typography.
    - Implemented CSS styles including responsive design for mobile and desktop.
    - Defined CSS variables for colors and font properties for consistency.
    - Structured layout using CSS Grid for better alignment and spacing.
    - Added media queries for responsive adjustments on smaller screens.

---
+++ main.js
@@ -0,0 +1,62 @@
+const container = document.getElementById("container");
+const textSection = container.querySelector(".text-section");
+const quote = container.querySelector(".quote");
+const authorInfo = container.querySelector(".author-info");
+const author = container.querySelector(".author");
+const field = container.querySelector(".field");
+const imageSection = container.querySelector(".image-section");
+const personImg = container.querySelector(".person-img");
+const controls = container.querySelector(".controls");
+const prevBtn = container.querySelector("#prev");
+const nextBtn = container.querySelector("#next");
+
+let current = 0;
+
+const quotes = [
+  {
+    id: 1,
+    name: "Tanya Sinclair",
+    field: "UX Engineer",
+    quote:
+      "I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.",
+    image: "/images/image-tanya.jpg",
+  },
+  {
+    id: 2,
+    name: "John Tarkpor",
+    field: "Junior Front-end Developer",
+    quote:
+      "If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
+    image: "/images/image-john.jpg",
+  },
+];
+
+function showQuote() {
+  quote.textContent = `"${quotes[current].quote}"`;
+  author.textContent = quotes[current].name;
+  field.textContent = quotes[current].field;
+  personImg.src = quotes[current].image;
+  personImg.alt = quotes[current].name;
+}
+function nextQuote() {
+  current++;
+  if (current >= quotes.length) {
+    current = 0;
+  }
+  showQuote();
+}
+function prevQuote() {
+  current--;
+  if (current < 0) {
+    current = quotes.length - 1;
+  }
+  showQuote();
+}
+nextBtn.addEventListener("click", function () {
+  nextQuote();
+});
+prevBtn.addEventListener("click", function () {
+  prevQuote();
+});
+console.log("Displaying quote for:", quotes[current].name);
+showQuote();
