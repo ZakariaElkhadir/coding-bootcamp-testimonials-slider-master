80fe96b1cfdf6da0197bd003f8a16ff5a0ec979f
ZakariaElkhadir <zelkhadir5@gmail.com>
2025-11-20 17:52:00 +0100

    Fix image paths and update quotes formatting in main.js and style.css

--- main.js
+++ main.js
@@ -18,16 +18,16 @@ const quotes = [
     name: "Tanya Sinclair",
     field: "UX Engineer",
     quote:
-      "I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.",
-    image: "/images/image-tanya.jpg",
+      "I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future.",
+    image: "./images/image-tanya.jpg",
   },
   {
     id: 2,
     name: "John Tarkpor",
     field: "Junior Front-end Developer",
     quote:
-      "If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
-    image: "/images/image-john.jpg",
+      "If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
+    image: "./images/image-john.jpg",
   },
 ];
 
